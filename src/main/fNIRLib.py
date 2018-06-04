fc02b85146e4e8ab8c97e444eec72941b19ea8cb
WIT\fiottokaufmanj <jadenfk@outlook.com>
2018-06-04 19:59:16 -0400

    Min max scaler

--- src/main/fNIRLib.py
+++ src/main/fNIRLib.py
@@ -1,4 +1,4 @@
-import tensorflow as tf
+from sklearn import preprocessing
 import pandas as pd
 import os
 
@@ -12,13 +12,19 @@ class fNIRLib:
                 subjects.append(pd.read_csv(data_path + subject + "/TRAIN_DATA", usecols=range(4,21), names=names))
         data = pd.Series(subjects)
         if combine:
-            data = pd.Series([pd.concat(data.values).reset_index()])
+            data = pd.Series([pd.concat(data.values)])
+        data = pd.Series([pd.Series([s]) for s in data])
         if points:
-            data = pd.Series([pd.Series([d.iloc[260 * x:260 * (x + 1),:] for x in range(d.shape[0] // 260)]) for d in data])
+            data = pd.Series([pd.Series([d.iloc[0].iloc[260 * x:260 * (x + 1),:] for x in range(d.iloc[0].shape[0] // 260)]) for d in data])
         return data
     @staticmethod
     def xySplit(data):
-        split_data = [[(y.iloc[:,:-1], y.iloc[0,-1]) for y in x] for x in data]
-        x_data = pd.Series([pd.Series([y[0] for y in x]) for x in split_data])
-        y_data = pd.Series([pd.Series([y[1] for y in x]) for x in split_data])
+        split_data = [(y.iloc[:,:-1], y.iloc[0,-1]) for y in data]
+        x_data = pd.Series([y[0] for y in split_data])
+        y_data = pd.Series([y[1] for y in split_data])
         return x_data, y_data
+    @staticmethod
+    def minMaxScale(data):
+        names = list(data.iloc[0])
+        scaler = preprocessing.MinMaxScaler(feature_range=(0,1))
+        return pd.Series([pd.DataFrame(scaler.fit_transform(y.values), columns=names) for y in data])
