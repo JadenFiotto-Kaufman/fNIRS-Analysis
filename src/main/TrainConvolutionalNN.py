3968014fe9474cfe7193a44f9633a718675f7391
corpandrew <corp@wit.edu>
2018-06-03 21:34:54 -0400

    traincnn first attempt Added a fix for fNIRLib on Mac

---
+++ src/main/TrainConvolutionalNN.py
@@ -0,0 +1,33 @@
+import numpy
+from keras.layers import Conv1D, GlobalAveragePooling1D, MaxPooling1D
+from keras.layers import Dense, Flatten
+from keras.models import Sequential
+
+from src.main.fNIRLib import fNIRLib
+
+trainData = fNIRLib.importData("/Users/corpa/BCI/processed/").get("p12")
+
+xTrain = trainData.iloc[:, 0:16]
+yTrain = trainData.iloc[:, 16]
+
+print(xTrain.head())
+
+print('y:')
+print(yTrain.head())
+
+model = Sequential()
+model.add(Conv1D(64, 2, activation='relu', input_shape=(16, 1)))
+model.add(MaxPooling1D())
+model.add(Conv1D(64, 2, activation='relu'))
+model.add(GlobalAveragePooling1D())
+Flatten()
+model.add(Dense(1, activation='sigmoid'))
+
+model.compile(loss='binary_crossentropy',
+              optimizer='rmsprop',
+              metrics=['accuracy'])
+
+xTrain = numpy.expand_dims(xTrain, axis=2)
+
+model.fit(xTrain, yTrain, batch_size=16, epochs=100)
+# score = model.evaluate(x_test, y_test, batch_size=16)
--- src/main/fNIRLib.py
+++ src/main/fNIRLib.py
@@ -8,5 +8,6 @@ class fNIRLib:
         TEST_subjects = {}
         names = ["a1HbO", "a1Hb", "a2HbO", "a2Hb", "a3HbO", "a3Hb", "a4HbO", "a4Hb", "b1HbO", "b1Hb", "b2HbO", "b2Hb", "b3HbO", "b3Hb", "b4HbO", "b4Hb", "Class"]
         for subject in os.listdir(data_path):
-            TEST_subjects[subject] = pd.read_csv(data_path + subject + "/TRAIN_DATA", usecols=range(4,21), names=names)
+            if subject != ".DS_Store":
+                TEST_subjects[subject] = pd.read_csv(data_path + subject + "/TRAIN_DATA", usecols=range(4,21), names=names)
         return TEST_subjects
