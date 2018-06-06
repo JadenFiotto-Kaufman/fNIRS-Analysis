9b6213843a0dcb06c2dbf4e92d43b1ed9b66c4c2
Jaden Fiotto-Kaufman <jadenfk@outlook.com>
2018-06-05 23:53:14 -0400

    Options to organize the data based on each reading Function to split the data into features and class

--- JadenTesting.py
+++ JadenTesting.py
@@ -1,6 +1,6 @@
 import numpy
-from keras.layers import LSTM, GlobalAveragePooling1D, MaxPooling1D, Dropout
-from keras.layers import Dense, Flatten
+from keras.layers import LSTM, Conv1D, Conv2D
+from keras.layers import Dense, Flatten, TimeDistributed
 from keras.models import Sequential
 
 from src.main.fNIRLib import fNIRLib
@@ -17,8 +17,9 @@ xTest = fNIRLib.to3D(xTest)
 input_shape = xTrain.shape
 
 model = Sequential()
-model.add(LSTM(50, input_shape=(input_shape[1], input_shape[2])))
-model.add(Dense(1))
+model.add(LSTM(50, activation='sigmoid', return_sequences=True, dropout=0.5, batch_input_shape=(68, input_shape[1], input_shape[2])))
+model.add(LSTM(50, activation='sigmoid'))
+model.add(Dense(1,activation='sigmoid'))
 model.compile(loss='binary_crossentropy', optimizer='adam', metrics=['accuracy'])
-model.fit(xTrain, yTrain, epochs=3000, batch_size=64)
+model.fit(xTrain, yTrain, epochs=5000, batch_size=68)
 
