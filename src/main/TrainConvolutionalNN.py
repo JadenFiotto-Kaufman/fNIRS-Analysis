f675fee8c3c094b0d5b88d4bffdefece3011d0a2
corpandrew <corp@wit.edu>
2018-06-07 12:48:39 -0400

    Merge branch 'master' of /Users/corpa/BCI with conflicts.

--- src/main/TrainConvolutionalNN.py
+++ src/main/TrainConvolutionalNN.py
@@ -1,28 +1,30 @@
 import numpy
-from keras.layers import Conv1D, GlobalAveragePooling1D, MaxPooling1D, Dropout
+from keras.layers import Conv1D, GlobalAveragePooling1D, MaxPooling1D, Dropout, Conv2D, MaxPooling2D, \
+    GlobalAveragePooling2D, Conv3D, MaxPooling3D
 from keras.layers import Dense, Flatten
 from keras.models import Sequential
 
 from src.main.fNIRLib import fNIRLib
 
-p12TrainData = fNIRLib.importData("/Users/corpa/BCI/processed/").get("p12")
+data = fNIRLib.importData("../../processed/", combine=True, points=True)
+features, classes = fNIRLib.xySplit(data.iloc[0])
+scaled = fNIRLib.minMaxScale(features)
 
-xTrain = p12TrainData.iloc[:, 0:16]
-yTrain = p12TrainData.iloc[:, 16]
+xTrain, xTest, yTrain, yTest = fNIRLib.testTrain(features,classes)
 
-p13TestData = fNIRLib.importData("/Users/corpa/BCI/processed/").get("p13")
-
-xTest = p13TestData.iloc[:, 0:16]
-yTest = p13TestData.iloc[:, 16]
-
+yTrain = yTrain.values
+yTest = yTest.values
+xTrain = fNIRLib.to3D(xTrain)
+xTest = fNIRLib.to3D(xTest)
 
 model = Sequential()
-model.add(Conv1D(64, 2, activation='relu', input_shape=(16, 1)))
-model.add(Conv1D(64, 2, activation='relu'))
-model.add(MaxPooling1D())
-model.add(Conv1D(128, 2, activation='relu'))
-model.add(Conv1D(128, 2, activation='relu'))
-model.add(GlobalAveragePooling1D())
+
+model.add(Conv2D(64, 2, activation='relu', input_shape=(input_shape[1], input_shape[2])))
+# model.add(Conv2D(64, 2, activation='relu'))
+model.add(MaxPooling2D())
+# model.add(Conv2D(128, 2, activation='relu'))
+model.add(Conv2D(128, 2, activation='relu'))
+# model.add(GlobalAveragePooling2D())
 Flatten()
 model.add(Dense(1, activation='sigmoid'))
 
@@ -30,22 +32,7 @@ model.compile(loss='binary_crossentropy',
               optimizer='rmsprop',
               metrics=['accuracy'])
 
-# was having trouble with input shape, now it should be correctly looking at the shape.
-xTrain = numpy.expand_dims(xTrain, axis=2)
-
-model.fit(xTrain, yTrain, batch_size=16, epochs=100)
+model.fit(xTrain, yTrain, batch_size=16, epochs=1000)
 
 scores = model.evaluate(xTrain, yTrain, verbose=0)
-print("%s: %.2f%%" % (model.metrics_names[1], scores[1]*100))
-
-# model_json = model.to_json()
-# with open("model.json", "w") as json_file:
-#     json_file.write(model_json)
-# # serialize weights to HDF5
-# model.save_weights("model.h5")
-# print("Saved model to disk")
-
-xTest = numpy.expand_dims(xTest, axis=2)
-
-score = model.evaluate(xTest, yTest, batch_size=16)
 print("%s: %.2f%%" % (model.metrics_names[1], scores[1]*100))
--- src/main/fNIRLib.py
+++ src/main/fNIRLib.py
@@ -1,69 +1,47 @@
 from sklearn import preprocessing
 import pandas as pd
-import random
-from numpy import array
+from numpy import random, arange, array
 import os
 from math import floor
+
 
 class fNIRLib:
     @staticmethod
     def importData(data_path, combine=False, points=False):
-        '''
-        Function to import each subject data 
-        :param data_path: relative path to /processed/ folder
-        :param combine: option to disregard differentiation of subjects and combine into one dataframe (recommended)
-        :param points: option to group each task into it's own data frame. This is every 260 time steps (recommended)
-        :return: Type of |Series(Series(DataFrame(2D)))| shape: (Subjects, Reading/Task, (Time Steps, Features))
-        '''
-        names = ["a1HbO", "a1Hb", "a2HbO", "a2Hb", "a3HbO", "a3Hb", "a4HbO", "a4Hb", "b1HbO", "b1Hb", "b2HbO", "b2Hb", "b3HbO", "b3Hb", "b4HbO", "b4Hb", "Class"]
-        train = [pd.read_csv(data_path + subject + "/TRAIN_DATA", usecols=range(4,21), names=names) for subject in os.listdir(data_path) if subject != ".DS_Store"]
-        test = [pd.read_csv(data_path + subject + "/TEST_DATA", usecols=range(4,21), names=names) for subject in os.listdir(data_path) if subject != ".DS_Store"]
-        data = pd.Series(train + test)
+        subjects = []
+        names = ["a1HbO", "a1Hb", "a2HbO", "a2Hb", "a3HbO", "a3Hb", "a4HbO", "a4Hb", "b1HbO", "b1Hb", "b2HbO", "b2Hb",
+                 "b3HbO", "b3Hb", "b4HbO", "b4Hb", "Class"]
+        for subject in os.listdir(data_path):
+            if subject != ".DS_Store":
+                subjects.append(pd.read_csv(data_path + subject + "/TRAIN_DATA", usecols=range(4, 21), names=names))
+        data = pd.Series(subjects)
         if combine:
             data = pd.Series([pd.concat(data.values)])
         data = pd.Series([pd.Series([s]) for s in data])
         if points:
-            data = pd.Series([pd.Series([d.iloc[0].iloc[260 * x:260 * (x + 1),:] for x in range(d.iloc[0].shape[0] // 260)]) for d in data])
+            data = pd.Series(
+                [pd.Series([d.iloc[0].iloc[260 * x:260 * (x + 1), :] for x in range(d.iloc[0].shape[0] // 260)]) for d
+                 in data])
         return data
+
     @staticmethod
     def xySplit(data):
-        '''
-        Splits data into features and classes
-        :param data: Type of |Series(DataFrames(2D))| shape: (Reading/Task, (Time Steps, Features))
-        :return: Type of |Series(Series(DataFrame(2D)))| shape: (Subjects, Reading/Task, (Time Steps, Features))
-        '''
-        split_data = [(y.iloc[:,:-1], y.iloc[0,-1]) for y in data]
+        split_data = [(y.iloc[:, :-1], y.iloc[0, -1]) for y in data]
         x_data = pd.Series([y[0] for y in split_data])
         y_data = pd.Series([y[1] for y in split_data])
         return x_data, y_data
+
     @staticmethod
     def minMaxScale(data):
-        '''
-        Scales data based on all readings
-        :param data: Type of |Series(DataFrames(2D))| shape: (Reading/Task, (Time Steps, Features))
-        :return: Type of |Series(Series(DataFrame(2D)))| shape: (Subjects, Reading/Task, (Time Steps, Features))
-        '''
         names = list(data.iloc[0])
-        scaler = preprocessing.MinMaxScaler(feature_range=(0,1))
-        scaler = scaler.fit(pd.concat([d for d in data]).values)
-        return pd.Series([pd.DataFrame(scaler.transform(y.values), columns=names) for y in data])
+        scaler = preprocessing.MinMaxScaler(feature_range=(0, 1))
+        return pd.Series([pd.DataFrame(scaler.fit_transform(y.values), columns=names) for y in data])
+
     @staticmethod
     def to3D(data):
-        '''
-        Converts data from pandas dataset format to numpy array format
-        :param data: Type of |Series(DataFrames(2D))| shape: (Reading/Task, (Time Steps, Features))
-        :return: Type of |Numpy.array(3D)| shape: (Subjects, Reading/Task, (Time Steps, Features))
-        '''
         return array([x.values for x in data])
+
     @staticmethod
-    def testTrain(features, classes, size=1./3.):
-        '''
-        Divides data into test and train based on percentage reserved for testing
-        :param features: Type of |Series(DataFrames(2D))| shape: (Reading/Task, (Time Steps, Features))
-        :param classes: Type of |Series()| shape: (Classes)
-        :param size: Percent of data reserved for testing
-        :return: Train features, test features, train classes, test classes
-        '''
-        n = features.size
-        index = random.sample(range(n), int(size * n))
-        return features.drop(index), features.iloc[index], classes.drop(index), classes.iloc[index]
+    def testTrain(features, classes, size=1. / 3.):
+        test = features.sample(frac=size, random_state=200)
+        return features.drop(test.index), test, classes.drop(test.index), classes.iloc[test.index]
