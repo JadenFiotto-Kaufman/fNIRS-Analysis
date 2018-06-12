eea3039cc2027a48df47fa36f55c0f0275d0df90
corpandrew <corp@wit.edu>
2018-06-12 12:53:39 -0400

    Issues with input shape

--- src/main/TrainConvolutionalNN.py
+++ src/main/TrainConvolutionalNN.py
@@ -1,8 +1,7 @@
-import numpy
-from keras.layers import Conv1D, GlobalAveragePooling1D, MaxPooling1D, Dropout, Conv2D, MaxPooling2D, \
-    GlobalAveragePooling2D, Conv3D, MaxPooling3D
-from keras.layers import Dense, Flatten
+from keras.layers import Conv1D, MaxPooling1D, InputLayer, Dense, GlobalAveragePooling1D
 from keras.models import Sequential
+
+import numpy as np
 
 from src.main.fNIRLib import fNIRLib
 
@@ -10,29 +9,34 @@ data = fNIRLib.importData("../../processed/", combine=True, points=True)
 features, classes = fNIRLib.xySplit(data.iloc[0])
 scaled = fNIRLib.minMaxScale(features)
 
-xTrain, xTest, yTrain, yTest = fNIRLib.testTrain(features,classes)
+xTrain, xTest, yTrain, yTest = fNIRLib.testTrain(features, classes)
 
 yTrain = yTrain.values
 yTest = yTest.values
+
 xTrain = fNIRLib.to3D(xTrain)
 xTest = fNIRLib.to3D(xTest)
+input_shape = xTrain.shape
+
+print(yTrain.shape)
+print(xTrain.shape)
 
 model = Sequential()
-
-model.add(Conv2D(64, 2, activation='relu', input_shape=(input_shape[1], input_shape[2])))
+model.add(Conv1D(64, kernel_size=3, activation='relu', batch_input_shape=(340, 16, 260)))
 # model.add(Conv2D(64, 2, activation='relu'))
-model.add(MaxPooling2D())
+model.add(MaxPooling1D())
 # model.add(Conv2D(128, 2, activation='relu'))
-model.add(Conv2D(128, 2, activation='relu'))
-# model.add(GlobalAveragePooling2D())
-Flatten()
-model.add(Dense(1, activation='sigmoid'))
+model.add(Conv1D(64, kernel_size=3, activation='relu'))
+model.add(GlobalAveragePooling1D())
+model.add(Dense(1, activation='linear'))
 
 model.compile(loss='binary_crossentropy',
               optimizer='rmsprop',
               metrics=['accuracy'])
 
+model.summary()
+
 model.fit(xTrain, yTrain, batch_size=16, epochs=1000)
 
-scores = model.evaluate(xTrain, yTrain, verbose=0)
-print("%s: %.2f%%" % (model.metrics_names[1], scores[1]*100))
+# scores = model.evaluate(xTrain, yTrain, verbose=0)
+# print("%s: %.2f%%" % (model.metrics_names[1], scores[1]*100))
