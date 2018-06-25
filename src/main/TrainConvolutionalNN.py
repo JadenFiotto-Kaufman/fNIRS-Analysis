f8643c60a917e7e34e30e14ff0db3f61d06e71b5
corpandrew <corp@wit.edu>
2018-06-25 00:17:09 -0400

    Updated Convolutional NN

--- src/main/TrainConvolutionalNN.py
+++ src/main/TrainConvolutionalNN.py
@@ -1,4 +1,4 @@
-from keras.layers import Conv1D, MaxPooling1D, InputLayer, Dense, GlobalAveragePooling1D
+from keras.layers import Conv1D, MaxPooling1D, InputLayer, Dense, GlobalAveragePooling1D, Dropout, AveragePooling1D
 from keras.models import Sequential
 
 import numpy as np
@@ -18,21 +18,27 @@ xTrain = fNIRLib.to3D(xTrain)
 xTest = fNIRLib.to3D(xTest)
 input_shape = xTrain.shape
 
+print(input_shape)
 
 model = Sequential()
-model.add(Conv1D(64, kernel_size=3, activation='relu', input_shape=(input_shape[1:])))
-# model.add(Conv2D(64, 2, activation='relu'))
-model.add(MaxPooling1D())
-# model.add(Conv2D(128, 2, activation='relu'))
-model.add(Conv1D(64, kernel_size=3, activation='relu'))
-model.add(GlobalAveragePooling1D())
+
+model.add(Conv1D(32, kernel_size=3, activation='relu', input_shape=(input_shape[1:])))
+model.add(MaxPooling1D(2))
+model.add(Conv1D(32, kernel_size=3, activation='relu'))
+model.add(AveragePooling1D(2))
+# model.add(Dropout(0.3))
+# model.add(Conv1D(64, kernel_size=3, activation='relu'))
+# model.add(AveragePooling1D(2))
+# model.add(Conv1D(32, kernel_size=3, activation='relu'))
+# model.add(GlobalAveragePooling1D())
 model.add(Dense(1, activation='linear'))
 
 model.compile(loss='binary_crossentropy',
               optimizer='rmsprop',
               metrics=['accuracy'])
 
-model.fit(xTrain, yTrain, batch_size=16, epochs=1000)
+model.fit(xTrain, yTrain, batch_size=16, epochs=500)
 
-# scores = model.evaluate(xTrain, yTrain, verbose=0)
-# print("%s: %.2f%%" % (model.metrics_names[1], scores[1]*100))
+scores = model.evaluate(xTrain, yTrain, verbose=0)
+print("%s: %.2f%%" % (model.metrics_names[1], scores[1]*100))
+
