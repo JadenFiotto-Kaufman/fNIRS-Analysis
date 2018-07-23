7a1df1af49e7dff4c22e32049e84f83448be274a
WIT\fiottokaufmanj <jadenfk@outlook.com>
2018-07-23 18:27:29 -0400

    Restructuring

--- src/main/fNIR.py
+++ src/main/fNIR.py
@@ -1,4 +1,5 @@
 from sklearn import svm, ensemble, preprocessing
+from sklearn.externals import joblib
 from keras.layers import Dense, MaxPooling1D, Dropout, LSTM, Conv1D
 from keras.models import Sequential
 from keras.callbacks import ModelCheckpoint
@@ -6,6 +7,7 @@ from src.main.fNIRLib import fNIRLib
 import pandas as pd
 from tsfresh import extract_relevant_features, feature_extraction
 from json import dump, load
+import os
 
 class fNIR:
     seed = 11
@@ -28,7 +30,7 @@ class fNIR:
             dump(featureNames, jsonFile)
         return True
     @staticmethod
-    def preprocess(filepath, combine=True, extract=False, test_size=.2, scale = True):
+    def preprocess(filepath, combine=True, extract=False, test_size=.2, scale=True):
         data = fNIRLib.importData(filepath, combine=combine)
         features, classes = fNIRLib.xySplit(data)
         if extract:
@@ -47,6 +49,8 @@ class fNIR:
         if scale:
             scaler = preprocessing.StandardScaler()
             features = pd.DataFrame(scaler.fit_transform(features), columns=list(features))
+            joblib.dump(scaler, "fNIRscaler.pkl")
+
         xTest, xTrain, yTest, yTrain = fNIRLib.testTrain(features, classes, size=test_size, seed=fNIR.seed)
         return {'xTest' : xTest,
                 'xTrain' : xTrain,
@@ -63,7 +67,6 @@ class fNIR:
         if load:
             model.load_weights(filepath)
         model.fit(data['xTrain'], data['yTrain'], epochs=epochs, batch_size=batch_size, validation_data=[data['xTest'], data['yTest']], callbacks=[checkpoint],shuffle=True)
-
     @staticmethod
     def train(filepath, method, epochs=5000, batch_size=3, combine=True, test_size=.2, load=False, scale=True):
         model = Sequential()
@@ -90,15 +93,104 @@ class fNIR:
             model.add(Dense(1, activation='sigmoid'))
             fNIR.neuralNet(model, data, epochs, batch_size, load)
         elif method == "SVM":
-            data = fNIR.preprocess(filepath, combine=True, extract=True, test_size=.2)
+            data = fNIR.preprocess(filepath, combine=True, extract=True, test_size=test_size, scale=scale)
             clf = svm.SVC()
             clf.fit(data['xTrain'].values, data['yTrain'].values)
             z = clf.predict(data['xTest'].values)
             print(sum(z == data['yTest'])/len(data['yTest']))
         elif method == "RDF":
-            data = fNIR.preprocess(filepath, combine=True, extract=True, test_size=.2)
+            data = fNIR.preprocess(filepath, combine=True, extract=True, test_size=test_size, scale=scale)
             clf = ensemble.RandomForestClassifier(max_depth=2, random_state=fNIR.seed)
             clf.fit(data['xTrain'].values, data['yTrain'].values)
             z = clf.predict(data['xTest'].values)
             print(sum(z == data['yTest']) / len(data['yTest']))
             pass
+    @staticmethod
+    def predother():
+        featurenames = None
+        with open("features_extracted.json") as data_file:
+            featurenames = load(data_file)
+
+        scaler = joblib.load("fNIRscaler.pkl")
+
+        # print(features)
+        model = Sequential()
+        model.add(Dense(25, input_dim=229, activation='relu'))
+        model.add(Dropout(.5))
+        model.add(Dense(20, activation='relu'))
+        model.add(Dropout(.5))
+        model.add(Dense(10, activation='relu'))
+        model.add(Dropout(.5))
+        model.add(Dense(10, activation='relu'))
+        model.add(Dropout(.5))
+        model.add(Dense(1, activation='sigmoid'))
+
+        model.load_weights('my_model_weights.hdf5')
+
+        data = fNIRLib.importData("../../processed/", combine=True)
+        features, classes = fNIRLib.xySplit(data)
+        features = pd.read_pickle('tsfeatures.pkl')
+        correct = 0
+        total = 0
+        for i,s in features.iterrows():
+            total = total + 1
+            s = pd.DataFrame(s).transpose()
+            s = pd.DataFrame(scaler.transform(s), columns=list(s))
+            prediction = model.predict_classes(s.values)
+            if prediction[0] == classes[i]:
+                correct = correct+1
+        print(total)
+        print(correct)
+    @staticmethod
+    def predict(weight_path='my_model_weights.hdf5'):
+
+        featurenames = None
+        with open("features_extracted.json") as data_file:
+            featurenames = load(data_file)
+
+        scaler = joblib.load("fNIRscaler.pkl")
+
+        #print(features)
+        model = Sequential()
+        model.add(Dense(25, input_dim=229, activation='relu'))
+        model.add(Dropout(.5))
+        model.add(Dense(20, activation='relu'))
+        model.add(Dropout(.5))
+        model.add(Dense(10, activation='relu'))
+        model.add(Dropout(.5))
+        model.add(Dense(10, activation='relu'))
+        model.add(Dropout(.5))
+        model.add(Dense(1, activation='sigmoid'))
+
+        model.load_weights(weight_path)
+
+        totalD = 0
+        totalE = 0
+        for subject in os.listdir("../../OUT"):
+
+            data = pd.read_csv("../../OUT/" + subject + "/UNSEEN_D", usecols=range(4, 21), names=fNIRLib.column_names)
+            data = data.tail(260)
+            features, classes = (data.iloc[:, :-1], data.iloc[0, -1])
+            features['id'] = [999] * 260
+            features['time'] = range(260)
+            features = feature_extraction.extract_features(features, kind_to_fc_parameters=featurenames, column_id="id",column_sort="time")
+            features = pd.DataFrame(scaler.transform(features), columns=list(features))
+            prediction = model.predict_classes(features)
+            if prediction[0] == classes:
+                totalD = totalD + 1
+
+            data = pd.read_csv("../../OUT/" + subject + "/UNSEEN_E", usecols=range(4, 21), names=fNIRLib.column_names)
+            data = data.tail(260)
+            features, classes = (data.iloc[:, :-1], data.iloc[0, -1])
+            features['id'] = [999] * 260
+            features['time'] = range(260)
+            features = feature_extraction.extract_features(features, kind_to_fc_parameters=featurenames, column_id="id",
+                                                           column_sort="time")
+            features = pd.DataFrame(scaler.transform(features), columns=list(features))
+            prediction = model.predict_classes(features)
+            if prediction[0] == classes:
+                totalE = totalE + 1
+        print(totalE)
+        print(totalD)
+
+
